74e5bf81c54f16f738421bc790b25557d748ec08
andreea gurzu <104028747+slayyyyyyy@users.noreply.github.com>
2023-11-22 14:12:48 +0200

    updated h5

--- Homework/Homework #5 - Psuedo-smart environment with sensors/Homework__5___Pseudo_smart_environment_with_sensors/Homework__5___Pseudo_smart_environment_with_sensors.ino
+++ Homework/Homework #5 - Psuedo-smart environment with sensors/Homework__5___Pseudo_smart_environment_with_sensors/Homework__5___Pseudo_smart_environment_with_sensors.ino
@@ -7,17 +7,19 @@ const int redRGBPin = 3;
 const int greenRGBPin = 5;
 const int blueRGBPin = 6;
 
-bool automaticMode = true; // Flag to control automatic mode for LED
-int minUltrasonicThreshold = 0; // Default threshold for ultrasonic sensor
-int minLDRThreshold = 0; // Default threshold for LDR sensor
-const unsigned long milliseconds = 1000; // Set default sampling interval to 1 second (1000 milliseconds)
-const int noOfAddresses = 20;
-const int noOfReadingsSaved = 10;
-const int minSamplingRate = 1;
-const int maxSamplingRate = 10;
+bool automaticMode = true; // flag to control automatic mode for LED
+
+int minUltrasonicThreshold = 0; 
+int minLDRThreshold = 0; 
 int LDRSensorValue = 0;
 int ultrasonicSensorValue = 0;
 
+const unsigned long milliseconds = 1000; // Set default sampling interval to 1 second (1000 milliseconds)
+const int noOfAddresses = 20; // number of eeprom addresses to save the sensor reading
+const int noOfReadingsSaved = 10;
+
+const int minSamplingRate = 1;
+const int maxSamplingRate = 10;
 int savedSamplingRate = 0;
 
 int redNormalLED = 0;
@@ -27,7 +29,13 @@ int redAlertLED = 0;
 int greenAlertLED = 0;
 int blueAlertLED = 0;
 
+const int noOfColorSettings = 6;
+
+const int minBrightness = 0;
+const int maxBrightness = 255;
+
 void writeColor(int r, int g, int b){
+  //function to write the RGB LED's color
     analogWrite(redRGBPin, r);
     analogWrite(greenRGBPin, g);
     analogWrite(blueRGBPin, b);
@@ -51,7 +59,7 @@ void setup() {
   pinMode(greenRGBPin, OUTPUT);
   pinMode(LDRPin, INPUT);
 
- handleMainMenu();
+  handleMainMenu();
 
 }
 
@@ -67,7 +75,7 @@ void readLDRSensorValue(){
 }
 
 void readUltrasonicSensorValue(){
-
+    /*aware of the use of magic numbers and the delays here, will fix it later*/
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
@@ -79,7 +87,8 @@ void readUltrasonicSensorValue(){
 }
 
 void readSamplingRate() {
-
+   /*this function checks if there's an already existing sampling rate in EEPROM, and if not
+   prompts the user to input one and saves it on address 0 */
     bool validInput = true;
 
     EEPROM.get(0, savedSamplingRate);
@@ -91,90 +100,97 @@ void readSamplingRate() {
         Serial.println("No valid sampling rate found in EEPROM. Enter a new value.");
         while (true) {
             Serial.println("Enter sampling rate (between 1 and 10 seconds): ");
-            while (!Serial.available()); // Wait for user input
+            while (!Serial.available()); // wait for user input
 
             savedSamplingRate = Serial.parseInt(); // Read the entered value
 
             if (savedSamplingRate >= minSamplingRate && savedSamplingRate <= maxSamplingRate) {
-                break; // Valid input, exit the loop
+                break; // valid input, exit the loop
             } else {
                 validInput = false;
                 Serial.println("Invalid input. Please enter a value between 1 and 10 seconds.");
             }
         }
 
-        EEPROM.put(0, savedSamplingRate); // Save valid sampling rate to EEPROM
+        EEPROM.put(0, savedSamplingRate); 
     }
 }
 
 
 void setMinUltrasonicThreshold() {
-
+    /*sets a minimum threshold for ultrasonic sensor from user input*/
     Serial.println("Enter threshold value for the ultrasonic sensor:");
-    while (!Serial.available()); // Wait for user input
+    while (!Serial.available()); // wait for user input
 
-    minUltrasonicThreshold = Serial.parseInt(); // Read the entered value
+    minUltrasonicThreshold = Serial.parseInt(); 
     Serial.print("Ultrasonic threshold set to: ");
     Serial.println(minUltrasonicThreshold);
     Serial.print("Input 4 to exit.");
 }
 
 void setMinLDRThreshold() {
-
+    /*sets a minimum threshold for ldr sensor from user input*/
     Serial.println("Enter threshold value for the LDR sensor:");
-    while (!Serial.available()); // Wait for user input
+    while (!Serial.available()); // wait for user input
 
-    minLDRThreshold = Serial.parseInt(); // Read the entered value
+    minLDRThreshold = Serial.parseInt(); 
     Serial.print("LDR threshold set to: ");
     Serial.println(minLDRThreshold);
     Serial.print("Input 4 to exit.");
 }
 
 void checkMinUltrasonicThreshold() {
+  /* checks if the current reading of the sensor exceeds the threshold given and lights up the LED accordingly*/
    while (!Serial.available()) {
     readUltrasonicSensorValue();
-    // Check if sensor values exceed thresholds
     if (ultrasonicSensorValue < minUltrasonicThreshold) {
         Serial.println("Ultrasonic sensor value exceeded threshold! Alert!");
         if (automaticMode) {
-            // Turn LED red
-            writeColor(255, 0, 0);
+            writeColor(maxBrightness, minBrightness, minBrightness);
         }
         else{
-          lastSavedAlertLEDColor();
+          lastSavedAlertLEDColor(); //if automatic mode is off, uses user RGB control inputs
         }
     }
 
     if (ultrasonicSensorValue >= minUltrasonicThreshold) {
         if(automaticMode){
-        writeColor(0, 255, 0);
+        writeColor(minBrightness, maxBrightness, minBrightness);
     }
     else{
       lastSavedNormalLEDColor();
+      }
     }
-    }
-   }
+  }
 }
 
 void checkMinLDRThreshold(){
+  /* checks if the current reading of the sensor exceeds the threshold given and lights up the LED accordingly*/
   while (!Serial.available()) {
    readLDRSensorValue();
    if (LDRSensorValue < minLDRThreshold) {
         Serial.println("LDR sensor value exceeded threshold! Alert!");
         if (automaticMode) {
-            // Turn LED red
-            writeColor(255, 0, 0);
+            writeColor(maxBrightness, minBrightness, minBrightness);
+        }
+        else{
+          lastSavedAlertLEDColor(); //if automatic mode is off, uses user RGB control inputs
         }
     }
 
-    if (automaticMode && LDRSensorValue >= minLDRThreshold) {
-        // No alerts, set LED to green
-        writeColor(0, 255, 0);
+    if (LDRSensorValue >= minLDRThreshold) {
+      if(automaticMode){
+          writeColor(minBrightness, maxBrightness, minBrightness);
+      }
+      else{
+          lastSavedNormalLEDColor(); //if automatic mode is off, uses user RGB control inputs
+      }
     }
   }
 }
 
 void confirmDataDeletion() {
+  /*resets all data to 0 */
   automaticMode = true; 
   minUltrasonicThreshold = 0; 
   minLDRThreshold = 0;
@@ -195,12 +211,13 @@ void confirmDataDeletion() {
   redAlertLED = 0;
   greenAlertLED = 0;
   blueAlertLED = 0;
-  writeColor(0,0,0);
+  writeColor(minBrightness,minBrightness,minBrightness);
   Serial.println("All data has been reseted.");
 }
 
 
 void readAndPrintSensorValues() {
+  /*reads the current sensor values and displays them at the set sampling rate. also aware of the delay used, will fix later*/
     int eepromAddress = 1;
     int samplingRate = savedSamplingRate * milliseconds;
     Serial.println("Input anything to stop");
@@ -209,30 +226,29 @@ void readAndPrintSensorValues() {
         readUltrasonicSensorValue();
         readLDRSensorValue();
 
-        // Print sensor readings
         Serial.print("Ultrasonic Sensor Value: ");
         Serial.println(ultrasonicSensorValue);
         Serial.print("LDR Sensor Value: ");
         Serial.println(LDRSensorValue);
 
-        // Delay to achieve the desired sampling rate
+        // delay to achieve the desired sampling rate
         delay(samplingRate);
 
-        // Save sensor readings to EEPROM
+        // save sensor readings to EEPROM
         EEPROM.put(eepromAddress, ultrasonicSensorValue);
         eepromAddress += sizeof(int);
         EEPROM.put(eepromAddress, LDRSensorValue);
         eepromAddress += sizeof(int);
 
 
-        // If we reach the end of the EEPROM, loop back to the beginning
+        // if we reach the end of the EEPROM, loop back to the beginning
         if (eepromAddress >= noOfAddresses * sizeof(int)) {
             eepromAddress = 1;
         }
 
     }
 
-    // Clear the serial input buffer
+    // clear the serial input buffer
     while (Serial.available()) {
         Serial.read();
     }
@@ -241,24 +257,21 @@ void readAndPrintSensorValues() {
 }
 
 void displayLast10SensorReadings() {
+  /*reads from eeprom and displays the last 10 sensor readings*/
     Serial.println("Last 10 sensor readings:");
     Serial.println("------------------------");
 
-    int eepromAddress = 1; // Start reading from the beginning
+    int eepromAddress = 1; 
 
-    // Loop through the last 10 sets of sensor readings in EEPROM
+    // loop through the last 10 sets of sensor readings in EEPROM
     for (int i = eepromAddress; i <= noOfReadingsSaved; ++i) {
 
         int ultrasonicData, LDRData;
-        // Read ultrasonic sensor data from EEPROM
         EEPROM.get(eepromAddress, ultrasonicData);
         eepromAddress += sizeof(int);
-
-        // Read LDR sensor data from EEPROM
         EEPROM.get(eepromAddress, LDRData);
         eepromAddress += sizeof(int);
 
-        // Display sensor readings
         Serial.print("Reading ");
         Serial.print(i);
         Serial.print(": Ultrasonic - ");
@@ -270,6 +283,7 @@ void displayLast10SensorReadings() {
 
 
 void displaySensorSettings() {
+
     Serial.println("\nCurrent Sensor Settings:");
     Serial.print("Sampling Rate: ");
     Serial.print("Every ");
@@ -287,22 +301,24 @@ void displaySensorSettings() {
 
 
 void toggleAutomaticMode() {
-    automaticMode = !automaticMode; // Toggle automatic mode
+  /*this function toggles the automatic mode on or off*/
+    automaticMode = !automaticMode; 
     Serial.print("Automatic Mode ");
     Serial.println(automaticMode ? "ON" : "OFF");
 }
 
 
 void setManualRGBColor() {
+  /*sets new color variables to modify LED colors for sensor readings for when automatic mode is off*/
     int colorIndex = 0;
     
     Serial.println("Enter value for red when the sensor doesn't exceed the threshold:");
 
-    while (colorIndex < 6) {
+    while (colorIndex < noOfColorSettings) {
+
         if (Serial.available()) {
             int value = Serial.parseInt();
-
-            if (value >= 0 && value <= 255) {
+            if (value >= minBrightness && value <= maxBrightness) {
                 switch (colorIndex) {
                     case 0:
                         redNormalLED = value;
@@ -331,7 +347,7 @@ void setManualRGBColor() {
                 colorIndex++;
             } else {
                 Serial.println("Invalid input. Enter a value between 0 and 255.");
-                // Clear the input buffer
+                // clears the input buffer
                 while (Serial.available()) {
                     Serial.read();
                 }
@@ -342,11 +358,11 @@ void setManualRGBColor() {
 }
 
 
+// the following functions will handle navigation through the main menu and the submenus
 
 
 void handleMainMenu() {
-  // TODO: Print the main menu options to the Serial Monitor
-  // The menu should list at least three options for the user to choose from
+
   Serial.flush();
   Serial.println("\nMain Menu:");
   Serial.println("1. Sensor Settings");
@@ -357,6 +373,7 @@ void handleMainMenu() {
 }
 
 void handleSensorSubmenu(){
+
   Serial.println("\t1.1 Sensor Sampling Interval");
   Serial.println("\t1.2 Ultrasonic Alert Threshold");
   Serial.println("\t1.3 LDR Alert Threshold");
@@ -373,13 +390,13 @@ void handleSensorSubmenu(){
     case 3:
       setMinLDRThreshold();
       checkMinLDRThreshold();
-      writeColor(0,0,0);
+      writeColor(minBrightness,minBrightness,minBrightness);
       handleSensorSubmenu();
       break;
     case 2:
       setMinUltrasonicThreshold();
       checkMinUltrasonicThreshold();
-      writeColor(0,0,0);
+      writeColor(minBrightness,minBrightness,minBrightness);
       handleSensorSubmenu();
       break;
     case 1:
@@ -390,10 +407,10 @@ void handleSensorSubmenu(){
       Serial.println("Invalid option");
       break;
   }
-
 }
 
 void handleResetSubmenu(){
+
   Serial.println("\tDo you want to reset all logger data?");
   Serial.println("\t2.1 Yes.");
   Serial.println("\t2.2 No.");
@@ -414,7 +431,6 @@ void handleResetSubmenu(){
       Serial.println("Invalid option");
       break;
   }
-
 }
 
 void handleStatusSubmenu(){
@@ -448,7 +464,6 @@ void handleStatusSubmenu(){
       Serial.println("Invalid option");
       break;
   }
-  
 }
 
 void handleLEDSubmenu(){
@@ -477,10 +492,10 @@ void handleLEDSubmenu(){
       Serial.println("Invalid option");
       break;
   }
-
 }
 
 void handleMenus() {
+
   Serial.print("\n ");
   int option = Serial.parseInt();
   switch(option){
